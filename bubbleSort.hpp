7ca573b692f801e8fde48f2a3a8abc6249650b8a
Gustavo Antonio Martini <gutamen@live.com>
2023-10-27 10:17:34 -0300

    macedo macaco

--- bubbleSort.hpp
+++ bubbleSort.hpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <iostream>
 #include <vector>
 using namespace  std;
@@ -15,13 +16,38 @@ void BubbleSort::classic(){
     std::vector<double> vet;
     vet.reserve(qts);
 
+
+
     double aux = 0;
     for(int i = 0; i < qts; i++){
-        cin >> aux;
+        //cin >> aux;
+        aux = random();
         vet.push_back(aux);
     }
-    asm("mov rax, %[vet];"
+    
+
+    for(int i = 0, k = 0 ; i < vet.size(); i++){
+        
+        if (k == 2) {
+            cout << vet.at(i) << "\t\t" << endl;
+            k = 0;
+        }else{
+            cout << vet.at(i) << "\t\t";
+            k++;
+        }
+    }
+
+    double jegue = 0;
+    
+
+    asm("lea rax, %[vet];"
+        "mov rbx, [rax+8];"
+        "mov %[jegue], rbx;"
         :
-        :[vet] "m" (vet)
+        :[vet] "m" (vet.at(0)), [jegue] "m"(jegue) 
         :"cc");
+
+    cout << jegue << endl;
+
+
 }
